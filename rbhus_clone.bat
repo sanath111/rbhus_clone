442c802250e08c00e52f30c9e6281ecd10c87707
sanath111 <sanathshetty111@gmail.com>
2024-11-12 15:23:30 +0530

    fixes

--- rbhus_clone.bat
+++ rbhus_clone.bat
@@ -1,1 +1,27 @@
-C:\Users\aum\AppData\Local\Microsoft\WindowsApps\python3.12.exe d:/Repos/rbhus_clone/login_prompt.py %*
+@echo off
+setlocal enabledelayedexpansion
+
+rem Resolve the full path to the script
+set "SOURCE=%~f0"
+:loop
+for %%I in ("%SOURCE%") do (
+    if /i "%%~I" neq "%%~fI" (
+        rem SOURCE is a symlink, resolve the target
+        set "SOURCE=%%~fI"
+        goto loop
+    )
+)
+
+rem Get the directory of the script
+for %%I in ("%SOURCE%") do set "DIR=%%~dpI"
+set "DIR=%DIR:~0,-1%"
+
+rem Activate the virtual environment
+call "%DIR%\rbhus_clone-env\Scripts\activate.bat"
+
+rem Change to the script directory
+pushd "%DIR%"
+python "%DIR%\login_prompt.py" %*
+popd
+
+endlocal
