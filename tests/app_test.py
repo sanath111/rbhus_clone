05e8043401dcc7125511761279d27e1953ae1668
sanath111 <sanathshetty111@gmail.com>
2024-11-12 19:22:56 +0530

    fixes

--- tests/app_test.py
+++ tests/app_test.py
@@ -83,7 +83,8 @@ class appTest():
 
         sizes = ["8", "9", "10", "11", "12", "14", "16", "18", "20", "22", "24", "26", "28", "32", "36", "48", "72"]
         self.main_ui.fontSizeBox.addItems(sizes)
-        self.set_font(QFont("NudiParijatha"))
+        # self.set_font(QFont("NudiParijatha"))
+        self.main_ui.fontBox.setCurrentFont(QFont("NudiParijatha"))
         self.main_ui.fontBox.currentFontChanged.connect(self.set_font)
         self.main_ui.fontSizeBox.currentIndexChanged.connect(self.set_font_size)
         
