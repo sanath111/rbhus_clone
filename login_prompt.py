b5e2342b29a8f9dab33af30ed1080a11b754d500
sanath111 <sanathshetty111@gmail.com>
2024-11-14 19:48:48 +0530

    fixes

--- login_prompt.py
+++ login_prompt.py
@@ -73,6 +73,8 @@ class loginPrompt():
                     self.main_ui.messageLabel.setText("Password matched")
                     self.main_ui.close()
                     # subprocess.run(sys.executable + " " + rbhus_clone + " -u " + username, shell=True)
+                    print(f"Launching subprocess with: {sys.executable} {rbhus_clone} -u {username}")
+                    debug.info(f"Launching subprocess with: {sys.executable} {rbhus_clone} -u {username}")
                     subprocess.run([sys.executable, rbhus_clone, '-u', username], shell=False)
                     sys.exit()
                 else:
