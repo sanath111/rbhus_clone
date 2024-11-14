3d7ac9948a1a459e10d8de69da6a60f86bdd2d31
sanath111 <sanathshetty111@gmail.com>
2024-11-14 18:00:33 +0530

    fixes

--- new_project.py
+++ new_project.py
@@ -168,7 +168,8 @@ class newProject():
 
 
     def setupVersioning(self, folder_path):
-        folder_path = folder_path.replace("\\\\", "\\")
+        if os.name == 'nt':
+            folder_path = folder_path.replace("\\\\", "\\")
         cmd_separator = '&' if os.name == 'nt' else '&&'
 
         init_hg_cmd = (
