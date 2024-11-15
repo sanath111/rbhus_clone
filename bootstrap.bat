37692b000cdf75314c75f1fe0312b2fe28ebdf05
sanath111 <sanathshetty111@gmail.com>
2024-11-15 12:38:11 +0530

    fixes

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,9 +1,15 @@
 @echo off
 rmdir /s /q rbhus_clone-env
+rmdir /s /q dist
+rmdir /s /q build
+echo "----- Removed old environment  -------"
 python -m venv rbhus_clone-env
 call rbhus_clone-env\Scripts\activate.bat
 set PYTHONPATH=
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
+echo "----- Created python environment -------"
 pyinstaller --onefile rbhus_clone_launcher_win.py
-echo "----- Created venv -------"
+echo "----- Created launcher -------"
+deactivate
+echo "----- Deactivated environment -------"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-rm -rf rbhus_clone-env
+rm -rfv rbhus_clone-env dist build
+echo "----- Removed old environment  -------"
 python3 -m venv rbhus_clone-env
 source rbhus_clone-env/bin/activate
 unset PYTHONPATH
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+echo "----- Created python environment -------"
 pyinstaller --onefile rbhus_clone_launcher_lin.py
-echo "----- Created venv -------"
+echo "----- Created launcher -------"
+deactivate
+echo "----- Deactivated environment  -------"
