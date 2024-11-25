080d514b25e687dabfcf6a2e10b56c4ffed5d961
sanath111 <sanathshetty111@gmail.com>
2024-11-25 14:54:11 +0530

    fixes

--- constants.py
+++ constants.py
@@ -3,17 +3,19 @@
 
 import os
 
-root_folder = r"/home/sanath/Documents/rbhus_clone_root"
-template_folder = root_folder + os.sep + "template"
+db_params = {}
+
+if os.name == 'nt':
+    root_folder = r"D:\rbhus_clone_root"
+    db_params = {'host': "localhost", 'db': "test", 'port': 3306, 'user': "root", 'passwd': "password"}
+else:
+    root_folder = r"/home/sanath/Documents/rbhus_clone_root"
+    db_params = {'host': "mysqlserver", 'db': "test", 'port': 4501, 'user': "hruser", 'passwd': "5Dt09WXa"}
+
+template_folder = os.path.join(root_folder, "template")
 
 if not os.path.exists(root_folder):
     os.mkdir(root_folder)
 if not os.path.exists(template_folder):
     os.mkdir(template_folder)
 
-db_params = {}
-
-if os.name == 'nt':
-    db_params = {'host': "localhost", 'db': "test", 'port': 3306, 'user': "root", 'passwd': "password"}
-else:
-    db_params = {'host': "mysqlserver", 'db': "test", 'port': 4501, 'user': "hruser", 'passwd': "5Dt09WXa"}
