9c2fd2b364612e0bc9e7e19a8f66d92e5bb53820
sanath111 <sanathshetty111@gmail.com>
2024-12-06 20:05:39 +0530

    fixes

--- initialize.bat
+++ initialize.bat
@@ -2,15 +2,16 @@
 setlocal
 
 cd D:
+mkdir "rbhus_clone"
+cd "rbhus_clone"
 git clone https://github.com/sanath111/rbhus_clone.git
-cd rbhus_clone || exit /b
 git checkout v3.0
-cd .. || exit /b
+cd D:
 mkdir "rbhus_clone_root"
 powershell -command "Compress-Archive -Path 'rbhus_clone_root\*' -DestinationPath 'rbhus_clone_root\backup.zip' -Force"
 xcopy /E /I "rbhus_clone\template" "rbhus_clone_root\template"
 xcopy /E /I "rbhus_clone\database" "rbhus_clone_root\database"
-cd "rbhus_clone" || exit /b
+cd "rbhus_clone"
 call "bootstrap.bat"
 
 endlocal
