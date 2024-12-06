22b3806f94aac144c3632b7bcb6542e97d3f27a3
sanath111 <sanathshetty111@gmail.com>
2024-12-06 20:20:33 +0530

    fixes

--- initialize.bat
+++ initialize.bat
@@ -1,17 +1,35 @@
 @echo off
 setlocal
 
-cd D:
-mkdir "rbhus_clone"
-cd "rbhus_clone"
+REM Change to the D: drive and root directory
+cd /d D:\ || exit /b
+
+REM Clone the Git repository
 git clone https://github.com/sanath111/rbhus_clone.git
+
+REM Change to the cloned directory
+cd rbhus_clone || exit /b
+
+REM Checkout the specified version
 git checkout v3.0
-cd D:
+
+REM Change back to the previous directory
+cd .. || exit /b
+
+REM Create the rbhus_clone_root directory
 mkdir "rbhus_clone_root"
+
+REM Create a zip file of the contents of rbhus_clone_root
 powershell -command "Compress-Archive -Path 'rbhus_clone_root\*' -DestinationPath 'rbhus_clone_root\backup.zip' -Force"
+
+REM Copy the template and database directories to rbhus_clone_root
 xcopy /E /I "rbhus_clone\template" "rbhus_clone_root\template"
 xcopy /E /I "rbhus_clone\database" "rbhus_clone_root\database"
-cd "rbhus_clone"
+
+REM Change to the rbhus_clone directory again
+cd "rbhus_clone" || exit /b
+
+REM Execute the bootstrap.bat script
 call "bootstrap.bat"
 
 endlocal
