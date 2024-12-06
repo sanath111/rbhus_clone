e14fe8942ed0287bad971fcae075a48f1d7ba738
sanath111 <sanathshetty111@gmail.com>
2024-12-06 19:55:23 +0530

    fixes

--- initialize.bat
+++ initialize.bat
@@ -10,5 +10,7 @@ mkdir "D:\rbhus_clone_root"
 powershell -command "Compress-Archive -Path 'D:\rbhus_clone_root\*' -DestinationPath 'D:\rbhus_clone_root\backup.zip' -Force"
 xcopy /E /I "D:\rbhus_clone\template" "D:\rbhus_clone_root\template"
 xcopy /E /I "D:\rbhus_clone\database" "D:\rbhus_clone_root\database"
+cd "D:\rbhus_clone" || exit /b
+bash "D:\rbhus_clone\bootstrap.bat"
 
 endlocal
