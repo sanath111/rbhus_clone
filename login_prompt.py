c532854e87c99e03352ad765ee6fa225eedb62c0
sanath111 <sanathshetty111@gmail.com>
2024-12-11 11:26:09 +0530

    fixes

--- login_prompt.py
+++ login_prompt.py
@@ -45,7 +45,7 @@ class loginPrompt():
 
         #Show Window
         self.main_ui.show()
-        # self.main_ui.showFullScreen()
+        self.main_ui.showFullScreen()
         self.main_ui.update()
 
         qtRectangle = self.main_ui.frameGeometry()
