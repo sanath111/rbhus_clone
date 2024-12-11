944af150272ecfffe284d4d669e2f436239da982
sanath111 <sanathshetty111@gmail.com>
2024-12-11 16:37:34 +0530

    fixes

--- initialize.bat
+++ initialize.bat
@@ -1,35 +1,49 @@
 @echo off
 setlocal
 
-REM Change to the D: drive and root directory
-cd /d D:\ || exit /b
+:: Define variables
+set "username=kiosk-user"
+set "password=password"
+set "repo_url=https://github.com/sanath111/rbhus_clone.git"
+set "version_tag=v3.0"
+set "bootstrap_script=bootstrap.bat"
+set "root_dir=D:\"
+set "clone_dir=rbhus_clone"
+set "output_dir=rbhus_clone_root"
 
-REM Clone the Git repository
-git clone https://github.com/sanath111/rbhus_clone.git
+:: Ensure script is run as administrator
+whoami /groups | find "S-1-5-32-544" >nul || (
+    echo This script must be run as an administrator.
+    pause
+    exit /b
+)
 
-REM Change to the cloned directory
-cd rbhus_clone || exit /b
+:: Create a new user
+echo Creating new user...
+net user %username% %password% /add
+if %errorlevel% neq 0 (
+    echo Failed to create user. Exiting.
+    exit /b
+)
 
-REM Checkout the specified version
-git checkout v3.0
+:: Switch to the new user and execute commands
+echo Switching to user %username%...
+runas /user:%username% cmd /c ^
+    ^"cd /d %root_dir% && ^
+    git clone %repo_url% && ^
+    cd %clone_dir% && ^
+    git checkout %version_tag% && ^
+    cd .. && ^
+    mkdir %output_dir% && ^
+    powershell -command "Compress-Archive -Path '%output_dir%\*' -DestinationPath '%output_dir%\backup.zip' -Force" && ^
+    xcopy /E /I "%clone_dir%\template" "%output_dir%\template" /Y && ^
+    xcopy /E /I "%clone_dir%\database" "%output_dir%\database" /Y && ^
+    cd %clone_dir% && ^
+    call %bootstrap_script%^"
 
-REM Change back to the previous directory
-cd .. || exit /b
+:: Completion message
+echo All tasks completed successfully.
+pause
 
-REM Create the rbhus_clone_root directory
-mkdir "rbhus_clone_root"
-
-REM Create a zip file of the contents of rbhus_clone_root
-powershell -command "Compress-Archive -Path 'rbhus_clone_root\*' -DestinationPath 'rbhus_clone_root\backup.zip' -Force"
-
-REM Copy the template and database directories to rbhus_clone_root
-xcopy /E /I "rbhus_clone\template" "rbhus_clone_root\template" /Y
-xcopy /E /I "rbhus_clone\database" "rbhus_clone_root\database" /Y
-
-REM Change to the rbhus_clone directory again
-cd "rbhus_clone" || exit /b
-
-REM Execute the bootstrap.bat script
-call "bootstrap.bat"
-
+:: End the local environment
 endlocal
